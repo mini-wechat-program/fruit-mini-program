5bbbb93366f992a42248db5e42bf14083d02ed17
yulin <yulin@pinduoduo.com>
2018-12-09 22:44:52 +0800

    [种树小程序]类目和banner开发

---
+++ src/pages/fruit_goods/components/menue/data.js
@@ -0,0 +1,758 @@
+export const menuList = [
+  {
+    'optId': 0,
+    'optName': '推荐',
+    'selectedIcon': 'http://pddmaimg.pinduoduo.com/prod/2018-03-05/2de472608672cad37546218977019e27.png',
+    'unselectedIcon': 'http://pddmaimg.pinduoduo.com/prod/2018-03-05/95a5594a10b9757af50dccac1a0e50af.png',
+    'secondaryOpts': [],
+    'sp': 0
+  },
+  {
+    'optId': 14,
+    'optName': '女装',
+    'selectedIcon': 'http://t05img.yangkeduo.com/images/2018-03-09/d444a41c7dd439681be3310f8fc3dce8.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/da66477f08f39bbb5ae6d245176e3799.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 4742,
+        'secondaryOptName': '上装'
+      },
+      {
+        'secondaryOptId': 3376,
+        'secondaryOptName': '冬热卖'
+      },
+      {
+        'secondaryOptId': 52,
+        'secondaryOptName': '裤装'
+      },
+      {
+        'secondaryOptId': 3384,
+        'secondaryOptName': '大衣外套'
+      },
+      {
+        'secondaryOptId': 4728,
+        'secondaryOptName': '大厂直供'
+      },
+      {
+        'secondaryOptId': 997,
+        'secondaryOptName': '棉服棉衣'
+      },
+      {
+        'secondaryOptId': 2715,
+        'secondaryOptName': '针织毛衣'
+      },
+      {
+        'secondaryOptId': 315,
+        'secondaryOptName': '羽绒服'
+      },
+      {
+        'secondaryOptId': 5105,
+        'secondaryOptName': '裙装套装'
+      }
+    ]
+  },
+  {
+    'optId': 15,
+    'optName': '百货',
+    'selectedIcon': 'http://t04img.yangkeduo.com/images/2018-03-09/515074133810edf9459d2fe44931b4a3.png',
+    'unselectedIcon': 'http://t05img.yangkeduo.com/images/2018-03-01/8ed97ecaa0fbd7d9c2aade37cd0d89db.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 330,
+        'secondaryOptName': '纸品湿巾'
+      },
+      {
+        'secondaryOptId': 326,
+        'secondaryOptName': '厨房用品'
+      },
+      {
+        'secondaryOptId': 329,
+        'secondaryOptName': '日用百货'
+      },
+      {
+        'secondaryOptId': 41,
+        'secondaryOptName': '珠宝饰品'
+      },
+      {
+        'secondaryOptId': 277,
+        'secondaryOptName': '收纳整理'
+      },
+      {
+        'secondaryOptId': 328,
+        'secondaryOptName': '洗护清洁'
+      },
+      {
+        'secondaryOptId': 327,
+        'secondaryOptName': '家庭清洁'
+      },
+      {
+        'secondaryOptId': 40,
+        'secondaryOptName': '水杯餐具'
+      },
+      {
+        'secondaryOptId': 890,
+        'secondaryOptName': '绿植园艺'
+      }
+    ]
+  },
+  {
+    'optId': 1281,
+    'optName': '鞋包',
+    'selectedIcon': 'http://t09img.yangkeduo.com/images/2018-03-09/4748728fc8057b5a6aba87dba581ee94.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/8d43d4891e4526d1a99983f7c0d698c0.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 1284,
+        'secondaryOptName': '女鞋'
+      },
+      {
+        'secondaryOptId': 1283,
+        'secondaryOptName': '男鞋'
+      },
+      {
+        'secondaryOptId': 3305,
+        'secondaryOptName': '冬上新'
+      },
+      {
+        'secondaryOptId': 5221,
+        'secondaryOptName': '功能箱包'
+      },
+      {
+        'secondaryOptId': 3201,
+        'secondaryOptName': '靴子'
+      },
+      {
+        'secondaryOptId': 5219,
+        'secondaryOptName': '女包'
+      },
+      {
+        'secondaryOptId': 5220,
+        'secondaryOptName': '男包'
+      },
+      {
+        'secondaryOptId': 1782,
+        'secondaryOptName': '低帮鞋'
+      },
+      {
+        'secondaryOptId': 3198,
+        'secondaryOptName': '高帮鞋'
+      }
+    ]
+  },
+  {
+    'optId': 1,
+    'optName': '食品',
+    'selectedIcon': 'http://t10img.yangkeduo.com/images/2018-03-09/6a6625e8dbf9fe7681a9926c4fcadad0.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/5d85ab1240b5d57c98afbfb947d7bcad.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 5867,
+        'secondaryOptName': '地方馆'
+      },
+      {
+        'secondaryOptId': 2,
+        'secondaryOptName': '休闲零食'
+      },
+      {
+        'secondaryOptId': 5968,
+        'secondaryOptName': '茶酒冲饮'
+      },
+      {
+        'secondaryOptId': 667,
+        'secondaryOptName': '饼干糕点'
+      },
+      {
+        'secondaryOptId': 3290,
+        'secondaryOptName': '农货节'
+      },
+      {
+        'secondaryOptId': 124,
+        'secondaryOptName': '粮油速食'
+      },
+      {
+        'secondaryOptId': 96,
+        'secondaryOptName': '坚果蜜饯'
+      },
+      {
+        'secondaryOptId': 683,
+        'secondaryOptName': '乳制品'
+      },
+      {
+        'secondaryOptId': 95,
+        'secondaryOptName': '营养保健'
+      }
+    ]
+  },
+  {
+    'optId': 4,
+    'optName': '母婴',
+    'selectedIcon': 'http://t01img.yangkeduo.com/images/2018-03-09/5c9062fda73e005aba275da57eda245e.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/e3c10e700dedf8da22211fcb95804fda.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 25,
+        'secondaryOptName': '童装'
+      },
+      {
+        'secondaryOptId': 600,
+        'secondaryOptName': '童鞋'
+      },
+      {
+        'secondaryOptId': 2693,
+        'secondaryOptName': '冬上新'
+      },
+      {
+        'secondaryOptId': 5285,
+        'secondaryOptName': '玩具文娱'
+      },
+      {
+        'secondaryOptId': 5,
+        'secondaryOptName': '纸尿裤'
+      },
+      {
+        'secondaryOptId': 5156,
+        'secondaryOptName': '洗护喂养'
+      },
+      {
+        'secondaryOptId': 34,
+        'secondaryOptName': '孕妈专区'
+      },
+      {
+        'secondaryOptId': 5148,
+        'secondaryOptName': '童车童床'
+      },
+      {
+        'secondaryOptId': 4935,
+        'secondaryOptName': '奶粉辅食'
+      }
+    ]
+  },
+  {
+    'optId': 1282,
+    'optName': '内衣',
+    'selectedIcon': 'http://t07img.yangkeduo.com/images/2018-03-09/8c322fc4d7b700e6f3fea0d4de0e81f6.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/edf0f00286f8414812646ac3186e8f5d.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 5304,
+        'secondaryOptName': '保暖专区'
+      },
+      {
+        'secondaryOptId': 1296,
+        'secondaryOptName': '睡衣'
+      },
+      {
+        'secondaryOptId': 1293,
+        'secondaryOptName': '文胸'
+      },
+      {
+        'secondaryOptId': 1294,
+        'secondaryOptName': '内裤'
+      },
+      {
+        'secondaryOptId': 5300,
+        'secondaryOptName': '服饰配件'
+      },
+      {
+        'secondaryOptId': 1292,
+        'secondaryOptName': '女士内衣'
+      },
+      {
+        'secondaryOptId': 1291,
+        'secondaryOptName': '男士内衣'
+      },
+      {
+        'secondaryOptId': 1295,
+        'secondaryOptName': '袜子'
+      },
+      {
+        'secondaryOptId': 5301,
+        'secondaryOptName': '腕表眼镜'
+      }
+    ]
+  },
+  {
+    'optId': 18,
+    'optName': '电器',
+    'selectedIcon': 'http://t11img.yangkeduo.com/images/2018-03-09/0b6310449f55876abda97fedcab3c0ad.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/43d3853283a4f19fcc3e5f49fdb0d705.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 804,
+        'secondaryOptName': '饭煲电磁炉'
+      },
+      {
+        'secondaryOptId': 4789,
+        'secondaryOptName': '取暖电器'
+      },
+      {
+        'secondaryOptId': 3241,
+        'secondaryOptName': '洗衣机'
+      },
+      {
+        'secondaryOptId': 447,
+        'secondaryOptName': '厨房电器'
+      },
+      {
+        'secondaryOptId': 445,
+        'secondaryOptName': '个护健康'
+      },
+      {
+        'secondaryOptId': 1624,
+        'secondaryOptName': '冰箱空调'
+      },
+      {
+        'secondaryOptId': 444,
+        'secondaryOptName': '生活电器'
+      },
+      {
+        'secondaryOptId': 2467,
+        'secondaryOptName': '电视'
+      },
+      {
+        'secondaryOptId': 522,
+        'secondaryOptName': '厨卫大电器'
+      }
+    ]
+  },
+  {
+    'optId': 743,
+    'optName': '男装',
+    'selectedIcon': 'http://t05img.yangkeduo.com/images/2018-03-09/dabdc582c748f68e57a3649c37ad779f.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/308600186f2b4db25f37c5ba1309ad84.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 4855,
+        'secondaryOptName': '上装'
+      },
+      {
+        'secondaryOptId': 3297,
+        'secondaryOptName': '羽绒服'
+      },
+      {
+        'secondaryOptId': 3299,
+        'secondaryOptName': '棉服'
+      },
+      {
+        'secondaryOptId': 1220,
+        'secondaryOptName': '大衣夹克'
+      },
+      {
+        'secondaryOptId': 3301,
+        'secondaryOptName': '套装'
+      },
+      {
+        'secondaryOptId': 2590,
+        'secondaryOptName': '毛衣'
+      },
+      {
+        'secondaryOptId': 2213,
+        'secondaryOptName': '牛仔裤'
+      },
+      {
+        'secondaryOptId': 1272,
+        'secondaryOptName': '休闲裤'
+      },
+      {
+        'secondaryOptId': 2169,
+        'secondaryOptName': '衬衫'
+      }
+    ]
+  },
+  {
+    'optId': 2974,
+    'optName': '家具',
+    'selectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-10-18/945202b6c61414b45d47f7b9defeff6d.png',
+    'unselectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-10-18/ff678eae9e1e23bca0799f7240918960.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 5169,
+        'secondaryOptName': '卧室家具'
+      },
+      {
+        'secondaryOptId': 5170,
+        'secondaryOptName': '客厅门厅'
+      },
+      {
+        'secondaryOptId': 5172,
+        'secondaryOptName': '餐厅家具'
+      },
+      {
+        'secondaryOptId': 5171,
+        'secondaryOptName': '书房家具'
+      },
+      {
+        'secondaryOptId': 3053,
+        'secondaryOptName': '儿童家具'
+      },
+      {
+        'secondaryOptId': 3065,
+        'secondaryOptName': '办公家具'
+      },
+      {
+        'secondaryOptId': 5173,
+        'secondaryOptName': '户外庭院'
+      },
+      {
+        'secondaryOptId': 3038,
+        'secondaryOptName': '椅子凳子'
+      },
+      {
+        'secondaryOptId': 3019,
+        'secondaryOptName': '柜子架子'
+      }
+    ]
+  },
+  {
+    'optId': 13,
+    'optName': '水果',
+    'selectedIcon': 'http://t11img.yangkeduo.com/images/2018-03-09/ae9e978b99df9b966fd1374607d3dbdf.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-06-04/f6f8ae92010594653b4794de91abd551.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 140,
+        'secondaryOptName': '新鲜水果'
+      },
+      {
+        'secondaryOptId': 5121,
+        'secondaryOptName': '柑橘橙柚'
+      },
+      {
+        'secondaryOptId': 146,
+        'secondaryOptName': '海鲜水产'
+      },
+      {
+        'secondaryOptId': 2446,
+        'secondaryOptName': '瓜果桃梨'
+      },
+      {
+        'secondaryOptId': 151,
+        'secondaryOptName': '蔬菜及制品'
+      },
+      {
+        'secondaryOptId': 1041,
+        'secondaryOptName': '进口优选'
+      },
+      {
+        'secondaryOptId': 978,
+        'secondaryOptName': '品质肉禽'
+      },
+      {
+        'secondaryOptId': 149,
+        'secondaryOptName': '蛋及蛋制品'
+      },
+      {
+        'secondaryOptId': 2456,
+        'secondaryOptName': '熟食凉菜'
+      }
+    ]
+  },
+  {
+    'optId': 16,
+    'optName': '美妆',
+    'selectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/8cd625fc3f3bff05e47705f7c4ecfab3.png',
+    'unselectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/079bb3c2db6ffab150957bf4f63b266f.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 317,
+        'secondaryOptName': '面膜'
+      },
+      {
+        'secondaryOptId': 69,
+        'secondaryOptName': '面部护理'
+      },
+      {
+        'secondaryOptId': 505,
+        'secondaryOptName': '口红唇膏'
+      },
+      {
+        'secondaryOptId': 607,
+        'secondaryOptName': '美妆工具'
+      },
+      {
+        'secondaryOptId': 643,
+        'secondaryOptName': '美发护发'
+      },
+      {
+        'secondaryOptId': 509,
+        'secondaryOptName': '水乳面霜'
+      },
+      {
+        'secondaryOptId': 70,
+        'secondaryOptName': '个人洗护'
+      },
+      {
+        'secondaryOptId': 71,
+        'secondaryOptName': '彩妆香氛'
+      },
+      {
+        'secondaryOptId': 6001,
+        'secondaryOptName': '男士护理'
+      }
+    ]
+  },
+  {
+    'optId': 1543,
+    'optName': '手机',
+    'selectedIcon': 'http://t01img.yangkeduo.com/images/2018-03-09/3aa73292dd4df24357f6de8dcdf3079f.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-07-29/58df7d35ad6ad84e73f47bc53adc271f.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 1544,
+        'secondaryOptName': '手机'
+      },
+      {
+        'secondaryOptId': 2165,
+        'secondaryOptName': '壳膜/支架'
+      },
+      {
+        'secondaryOptId': 1546,
+        'secondaryOptName': '充电线/器'
+      },
+      {
+        'secondaryOptId': 1547,
+        'secondaryOptName': '耳机/耳麦'
+      },
+      {
+        'secondaryOptId': 1548,
+        'secondaryOptName': '音箱/音响'
+      },
+      {
+        'secondaryOptId': 1549,
+        'secondaryOptName': '手环/手表'
+      },
+      {
+        'secondaryOptId': 1551,
+        'secondaryOptName': '智能生活'
+      },
+      {
+        'secondaryOptId': 1545,
+        'secondaryOptName': '充电宝'
+      },
+      {
+        'secondaryOptId': 1550,
+        'secondaryOptName': '相机'
+      }
+    ]
+  },
+  {
+    'optId': 818,
+    'optName': '家纺',
+    'selectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/cda4e83e8713df30cf2d186e57f91e52.png',
+    'unselectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/26195496540c629966417812c23cd706.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 819,
+        'secondaryOptName': '四件套'
+      },
+      {
+        'secondaryOptId': 2718,
+        'secondaryOptName': '冬上新'
+      },
+      {
+        'secondaryOptId': 857,
+        'secondaryOptName': '床单被套'
+      },
+      {
+        'secondaryOptId': 5777,
+        'secondaryOptName': '床垫床褥'
+      },
+      {
+        'secondaryOptId': 5769,
+        'secondaryOptName': '被子毛毯'
+      },
+      {
+        'secondaryOptId': 823,
+        'secondaryOptName': '地毯窗帘'
+      },
+      {
+        'secondaryOptId': 5784,
+        'secondaryOptName': '枕头靠垫'
+      },
+      {
+        'secondaryOptId': 859,
+        'secondaryOptName': '毛巾沙发垫'
+      },
+      {
+        'secondaryOptId': 1023,
+        'secondaryOptName': '家居饰品'
+      }
+    ]
+  },
+  {
+    'optId': 1451,
+    'optName': '运动',
+    'selectedIcon': 'http://t04img.yangkeduo.com/images/2018-03-09/6047628f483038baf1be2983de80b831.png',
+    'unselectedIcon': 'http://omsproductionimg.yangkeduo.com/images/2017-07-10/9ac3dc6edb87f9eaabdfd5897aae2e92.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 1452,
+        'secondaryOptName': '运动鞋'
+      },
+      {
+        'secondaryOptId': 1453,
+        'secondaryOptName': '运动服装'
+      },
+      {
+        'secondaryOptId': 1455,
+        'secondaryOptName': '健身器材'
+      },
+      {
+        'secondaryOptId': 1454,
+        'secondaryOptName': '户外鞋服'
+      },
+      {
+        'secondaryOptId': 1456,
+        'secondaryOptName': '户外装备'
+      },
+      {
+        'secondaryOptId': 1457,
+        'secondaryOptName': '体育用品'
+      },
+      {
+        'secondaryOptId': 2171,
+        'secondaryOptName': '骑行装备'
+      },
+      {
+        'secondaryOptId': 2172,
+        'secondaryOptName': '垂钓装备'
+      },
+      {
+        'secondaryOptId': 3342,
+        'secondaryOptName': '秋冬保暖'
+      }
+    ]
+  },
+  {
+    'optId': 2478,
+    'optName': '电脑',
+    'selectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/c16a251c54bb211357a8febbb49c36e8.png',
+    'unselectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/e6da3d39c245761736bbb8688aa1bf9a.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 2479,
+        'secondaryOptName': '平板电脑'
+      },
+      {
+        'secondaryOptId': 3270,
+        'secondaryOptName': '学习机'
+      },
+      {
+        'secondaryOptId': 2481,
+        'secondaryOptName': '电脑整机'
+      },
+      {
+        'secondaryOptId': 2482,
+        'secondaryOptName': '笔类'
+      },
+      {
+        'secondaryOptId': 2483,
+        'secondaryOptName': '外设配件'
+      },
+      {
+        'secondaryOptId': 3260,
+        'secondaryOptName': '学生文具'
+      },
+      {
+        'secondaryOptId': 2485,
+        'secondaryOptName': '游戏设备'
+      },
+      {
+        'secondaryOptId': 2486,
+        'secondaryOptName': '办公设备'
+      },
+      {
+        'secondaryOptId': 2487,
+        'secondaryOptName': '网络设备'
+      }
+    ]
+  },
+  {
+    'optId': 2048,
+    'optName': '汽车',
+    'selectedIcon': 'http://t02img.yangkeduo.com/images/2018-03-09/264046059210a58fdbc384fe21194484.png',
+    'unselectedIcon': 'http://t08img.yangkeduo.com/images/2018-03-01/dddda3d710fc85d51d0e43fec254d3c4.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 2409,
+        'secondaryOptName': '座垫枕靠'
+      },
+      {
+        'secondaryOptId': 2050,
+        'secondaryOptName': '车用电子'
+      },
+      {
+        'secondaryOptId': 2410,
+        'secondaryOptName': '脚垫尾箱垫'
+      },
+      {
+        'secondaryOptId': 2051,
+        'secondaryOptName': '汽车零配件'
+      },
+      {
+        'secondaryOptId': 2049,
+        'secondaryOptName': '汽车内饰'
+      },
+      {
+        'secondaryOptId': 2052,
+        'secondaryOptName': '外饰防护'
+      },
+      {
+        'secondaryOptId': 2156,
+        'secondaryOptName': '清洗工具'
+      },
+      {
+        'secondaryOptId': 2053,
+        'secondaryOptName': '美容养护'
+      },
+      {
+        'secondaryOptId': 2054,
+        'secondaryOptName': '摩托车配件'
+      }
+    ]
+  },
+  {
+    'optId': 1917,
+    'optName': '家装',
+    'selectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/46e49fdb4e2d1a2195202510e5adadb6.png',
+    'unselectedIcon': 'http://t00img.yangkeduo.com/goods/images/2018-12-06/3b5f0c7705c4f3dcc135b6d092bec5d4.png',
+    'secondaryOpts': [
+      {
+        'secondaryOptId': 1942,
+        'secondaryOptName': '灯饰照明'
+      },
+      {
+        'secondaryOptId': 1943,
+        'secondaryOptName': '五金工具'
+      },
+      {
+        'secondaryOptId': 1947,
+        'secondaryOptName': '卫浴用品'
+      },
+      {
+        'secondaryOptId': 2904,
+        'secondaryOptName': '电子电工'
+      },
+      {
+        'secondaryOptId': 2918,
+        'secondaryOptName': '厨房'
+      },
+      {
+        'secondaryOptId': 2927,
+        'secondaryOptName': '监控安防'
+      },
+      {
+        'secondaryOptId': 2933,
+        'secondaryOptName': '墙地面'
+      },
+      {
+        'secondaryOptId': 2944,
+        'secondaryOptName': '基础建材'
+      },
+      {
+        'secondaryOptId': 2283,
+        'secondaryOptName': '全屋定制'
+      }
+    ]
+  }
+]
